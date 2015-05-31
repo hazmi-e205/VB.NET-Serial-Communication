8ee5809ba06525c4bdfc3a8ffb22af1988b39f7f
Hazmi F47 <hazmiarifin@gmail.com>
2015-05-31 14:24:53 +0700

    Update Program
    ada beberapa kode yang rubah, bagian delegate dan invokenya

--- Komunikasi Serial/Komunikasi Serial/Form1.vb
+++ Komunikasi Serial/Komunikasi Serial/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Delegate Sub ambildata(ByVal datanya As String)
+    Public getdata As New ambildata(AddressOf terimadata)
+
     Private Sub ComboBox1_Click(sender As Object, e As EventArgs) Handles ComboBox1.Click
         ComboBox1.Items.Clear()
         ComboBox1.Items.AddRange(IO.Ports.SerialPort.GetPortNames)
@@ -14,13 +16,16 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        SerialPort1.PortName = ComboBox1.Text
-        SerialPort1.BaudRate = ComboBox2.Text
-        SerialPort1.ReceivedBytesThreshold = TextBox1.Text
-        SerialPort1.ReadBufferSize = TextBox2.Text
-        SerialPort1.WriteBufferSize = TextBox3.Text
-        SerialPort1.Open()
-
+        Try
+            SerialPort1.PortName = ComboBox1.Text
+            SerialPort1.BaudRate = ComboBox2.Text
+            SerialPort1.ReceivedBytesThreshold = TextBox1.Text
+            SerialPort1.ReadBufferSize = TextBox2.Text
+            SerialPort1.WriteBufferSize = TextBox3.Text
+            SerialPort1.Open()
+        Catch ex As Exception
+            MsgBox("Pastikan Koneksi Anda Benar !!!", MsgBoxStyle.Exclamation, "Peringatan")
+        End Try
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -39,22 +44,15 @@
 
 
     Private Sub SerialPort1_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
-        terimadata(SerialPort1.ReadExisting())
+        Dim datanya As String = SerialPort1.ReadLine
+        Invoke(getdata, datanya)
     End Sub
 
 
     Private Sub terimadata(ByVal datanya As String)
-        If Me.InvokeRequired Then
-            Dim x As New ambildata(AddressOf terimadata)
-            Me.Invoke(x, New Object() {(datanya)})
-        Else
-            RichTextBox1.Text &= datanya
-            RichTextBox1.SelectionStart = RichTextBox1.TextLength
-            RichTextBox1.ScrollToCaret()
-        End If
+        RichTextBox1.Text &= datanya
+        RichTextBox1.SelectionStart = RichTextBox1.TextLength
+        RichTextBox1.ScrollToCaret()
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
